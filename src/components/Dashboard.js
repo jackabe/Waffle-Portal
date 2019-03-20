6bbf7719e3a8a824bfaadaf1a0e50fd243f42276
Morgan Jones <JonesM125@cardiff.ac.uk>
2019-03-20 10:16:31 +0000

    Setup Page, created a GET FETCH request to retrieve user insights.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,18 +8,47 @@ class Dashboard extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            
         };
     }
 
     componentDidMount() {
+        this.getInsights();
     }
+
+
+    getInsights(){
+
+        fetch('http://18.188.105.214/getInsights', {
+            method: 'get',
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        }).then(response => {
+
+        }).catch(error => {
+
+        })
+
+    }
+
 
     render() {
         return (
             <div className='dashboard'>
                 <h3 className='heading'>Dashboard</h3>
                 <div className='container'>
-                    
+                    <h4 className='heading'>The total number of vouchers redeemed today is...</h4>
+                    <p></p>
+
+                    <h4 className='heading'>The most popular store vouchers are being used at is...</h4>
+                    <p></p>
+
+                    <h4 className='heading'>The total number of bookings due today is...</h4>
+                    <p></p>
+
+                    <h4 className='heading'>The most popular car park is...</h4>
+                    <p></p>
                 </div>
             </div>
         );
