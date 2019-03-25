d06c676e9f852481e3d4eaf43f330b53113e6ba2
Morgan Jones <JonesM125@cardiff.ac.uk>
2019-03-25 16:30:24 +0000

    Updated calculations to compare the redeem dates

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react';
 import '../App.css';
 // import { ClipLoader } from 'react-spinners';
 // import FontAwesome from "react-fontawesome";
+import axios from "axios";
+
 
 class Dashboard extends Component {
-    // Initial Commit for CI
+
     constructor(props) {
         super(props);
         this.state = {
             dailyVouchers: '',
-            mcDaily: '',
-            mcTotal: '',
-            bkDaily: '',
-            bkTotal: '',
-            subDaily: '',
-            subTotal: '',
+            totalVouchers: '',
         };
     }
 
@@ -23,38 +20,39 @@ class Dashboard extends Component {
     }
 
 
-    getInsights(){
-
-        fetch('http://127.0.0.1/getAllOffers', {
+    getInsights = () => {
+        axios({
             method: 'get',
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            },
-
-        }).then(response => {
-            console.log(response.json())
-
-            let data = JSON.parse(response['_bodyText']);
-            this.processOfferInsights(data)
-
-        }).catch(error => {
-            console.log(error);
-            const { code, message } = error;
+            url: 'http://127.0.0.1/getAllOffers'
         })
+            .then((response) => {
+                let data = response.data;
+                this.processOfferInsights(data)
+            })
+            .catch((response) => {
+                console.log(response)
+            });
     }
 
     processOfferInsights(offerList){
         console.log("offers: " + offerList);
 
-        let dailyRedemptions = 0
-        let mcDaily = 0;
-        let bkDaily = 0;
-        let subDaily = 0;
-        let mcTotal = 0;
-        let bkTotal = 0;
-        let subTotal = 0;
+        let totalVouchers = 0;
+        let dailyVouchers = 0;
+        let today = new Date();
+        let dd = today.getDate();
+        let mm = today.getMonth()+1; //January is 0!
+        let yyyy = today.getFullYear();
 
-        let today = new Date().getDate();
+        if(dd<10) {
+            dd = '0'+dd
+        }
+
+        if(mm<10) {
+            mm = '0'+mm
+        }
+
+        let string_today = dd + '-' + mm + '-' + yyyy;
 
         let i = 0;
         for (i; i < offerList.length; i++){
@@ -65,40 +63,23 @@ class Dashboard extends Component {
                 expiry: offerList[i]['expiry_date'],
                 offerId: offerList[i]['offer_id'],
                 redemptionDate: offerList[i]['redemption_date'],
-                scans: offerList[i]['scans']
+                scans: offerList[i]['scans'],
+                redeem: offerList[i]['redeem'],
             };
 
-            // Converting String to date.
-            let parts = offer.redemptionDate.split("-");
-            let date = new Date(parts[0], parts[1] - 1, parts[2]);
 
-            if (offer.company === "McDonalds"){
-                mcTotal += 1;
-                if(date = today){
-                    dailyRedemptions += 1
-                    mcDaily += 1
-                }
-            }else if(offer.company === "Subway"){
-                subTotal += 1;
-                if(date = today){
-                    dailyRedemptions += 1
-                    subDaily += 1
-                }
-            }else{
-                bkTotal += 1;
-                if(date = today){
-                    dailyRedemptions += 1
-                    bkDaily += 1
-                }
+            console.log("redeem " + offer.redemptionDate);
+            console.log("today " + string_today);
+
+            if ( offer.redemptionDate == string_today && offer.redeem === true){
+                dailyVouchers += 1;
+                totalVouchers += 1;
+            }else if (offer.redeem === true){
+                totalVouchers += 1;
             }
 
-            // this.setState({dailyVouchers: dailyRedemptions});
-            // this.setState({mcDaily: mcDaily});
-            // this.setState({bkDaily: bkDaily});
-            // this.setState({subDaily: subDaily});
-            // this.setState({mcTotal: mcTotal});
-            // this.setState({bkTotal: bkTotal});
-            // this.setState({subTotal: subTotal});
+            this.setState({dailyVouchers: dailyVouchers});
+            this.setState({totalVouchers: totalVouchers});
         }
     }
 
@@ -107,18 +88,22 @@ class Dashboard extends Component {
             <div className='dashboard'>
                 <h3 className='heading'>Dashboard</h3>
                 <div className='container'>
-                    <h4 className='heading'>The total number of vouchers redeemed today is...</h4>
-                    <p>{this.state.dailyVouchers}</p>
+                    <div className='Row'>
+                        <div className='insight-circle'>
+                            <h4>Number of offers redeemed today:</h4>
+                            <h1>{this.state.dailyVouchers}</h1>
+                        </div>
 
-                    <h4 className='heading'>The most popular store vouchers are being used at is...</h4>
-                    <p></p>
+                        <div className='insight-circle'>
+                            <h4>Number of offers redeemed:</h4>
+                            <h1>{this.state.totalVouchers}</h1>
+                        </div>
 
-                    <h4 className='heading'>The total number of bookings due today is...</h4>
-                    <p></p>
 
-                    <h4 className='heading'>The most popular car park is...</h4>
-                    <p></p>
+                    </div>
                 </div>
+
+
             </div>
         );
     }
