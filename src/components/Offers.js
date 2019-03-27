ed1d652a5792a675e886145389817134d84e1f3b
Morgan Jones <JonesM125@cardiff.ac.uk>
2019-03-27 11:15:21 +0000

    Created AJAX request to retrieve a list of offers.

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../App.css';
+import axios from "axios";
 import { ClipLoader } from 'react-spinners';
 import FontAwesome from "react-fontawesome";
 
@@ -8,11 +9,51 @@ class Offers extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            offers: [],
+
         };
     }
 
     componentDidMount() {
+        this.getOffers();
     }
+
+
+    getOffers = () => {
+        axios({
+            method: 'get',
+            url: 'http://18.188.105.214/getAllOffers'
+        })
+            .then((response) => {
+                let data = response.data;
+                let i= 0;
+                let offers = [];
+
+                for(i; i < data.length; i++){
+                    let offer = {
+                        logo: data[i]['logo'],
+                        company: data[i]['store'],
+                        offer: data[i]['offer'],
+                        expiry: data[i]['expiry_date'],
+                        offerId: data[i]['offer_id'],
+                        redemptionDate: data[i]['redemption_date'],
+                        scans: data[i]['scans'],
+                        redeem: data[i]['redeem'],
+                    };
+                    offers.push(offer);
+                    if(i === offers.length - 1){
+                        this.setState({offers: offers});
+
+                    }
+                }
+
+            })
+            .catch((response) => {
+                console.log(response)
+            });
+    }
+
+
 
     render() {
         return (
